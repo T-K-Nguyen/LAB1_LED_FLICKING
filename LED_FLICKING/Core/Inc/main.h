70a48f300a95ccf90b22be1685e7adc6ca27249c
t_k_nguyen-admin <thuan.nguyen26104bk@hcmut.edu.vn>
2024-09-20 11:54:02 +0700

    finishing ex6

--- LED_FLICKING/Core/Inc/main.h
+++ LED_FLICKING/Core/Inc/main.h
@@ -58,10 +58,48 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
-#define LED_RED_Pin GPIO_PIN_5
-#define LED_RED_GPIO_Port GPIOA
-#define LED_YELLOW_Pin GPIO_PIN_6
-#define LED_YELLOW_GPIO_Port GPIOA
+#define ONE_Pin GPIO_PIN_4
+#define ONE_GPIO_Port GPIOA
+#define TWO_Pin GPIO_PIN_5
+#define TWO_GPIO_Port GPIOA
+#define THREE_Pin GPIO_PIN_6
+#define THREE_GPIO_Port GPIOA
+#define FOUR_Pin GPIO_PIN_7
+#define FOUR_GPIO_Port GPIOA
+#define LED_F2_Pin GPIO_PIN_12
+#define LED_F2_GPIO_Port GPIOB
+#define LED_G2_Pin GPIO_PIN_13
+#define LED_G2_GPIO_Port GPIOB
+#define FIVE_Pin GPIO_PIN_8
+#define FIVE_GPIO_Port GPIOA
+#define SIX_Pin GPIO_PIN_9
+#define SIX_GPIO_Port GPIOA
+#define SEVEN_Pin GPIO_PIN_10
+#define SEVEN_GPIO_Port GPIOA
+#define EIGHT_Pin GPIO_PIN_11
+#define EIGHT_GPIO_Port GPIOA
+#define NINE_Pin GPIO_PIN_12
+#define NINE_GPIO_Port GPIOA
+#define TEN_Pin GPIO_PIN_13
+#define TEN_GPIO_Port GPIOA
+#define ELVEN_Pin GPIO_PIN_14
+#define ELVEN_GPIO_Port GPIOA
+#define TWELVE_Pin GPIO_PIN_15
+#define TWELVE_GPIO_Port GPIOA
+#define LED_D_Pin GPIO_PIN_3
+#define LED_D_GPIO_Port GPIOB
+#define LED_E_Pin GPIO_PIN_4
+#define LED_E_GPIO_Port GPIOB
+#define LED_F_Pin GPIO_PIN_5
+#define LED_F_GPIO_Port GPIOB
+#define LED_G_Pin GPIO_PIN_6
+#define LED_G_GPIO_Port GPIOB
+#define LED_A2_Pin GPIO_PIN_7
+#define LED_A2_GPIO_Port GPIOB
+#define LED_B2_Pin GPIO_PIN_8
+#define LED_B2_GPIO_Port GPIOB
+#define LED_C2_Pin GPIO_PIN_9
+#define LED_C2_GPIO_Port GPIOB
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
